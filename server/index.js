8e529ac58e236005c9aa6bc0a631db0b5fcb95c9
gauravvermaqbs <gaurav.verma2@qbslearning.com>
2023-04-04 18:28:54 +0530

    text validation api added

--- server/index.js
+++ server/index.js
@@ -24,9 +24,25 @@ app.post("/translate", async (req, res) => {
     model: "text-davinci-003",
     prompt: `Translate this into ${language}: ${message}`,
     max_tokens: 2048,
-    temperature: 1,
+    temperature: 0.7,
   });
   res.send(response.data.choices[0].text);
+});
+
+app.post("/textValidation", async (req, res) => {
+  const { content, api_key,prompt } = req.body;
+  // console.log(content)
+  const configuration = new Configuration({
+    apiKey: api_key,
+  });
+  const openai = new OpenAIApi(configuration);
+  const correct_response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${prompt} ${content}`,
+    max_tokens: 2048,
+    temperature: 1,
+  });
+  res.send(correct_response.data.choices[0].text);
 });
 
 app.listen(PORT, () => {

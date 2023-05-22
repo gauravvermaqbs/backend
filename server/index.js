4f7cd47e9ef92d68c4fc3d3f4ad800ca5517c9ec
gauravvermaqbs <gaurav.verma2@qbslearning.com>
2023-05-22 19:17:49 +0530

    origin changed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: ['http://localhost:3000','https://ai-automation-production.netlify.app','https://ai.qbslearning.com/'] }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -34,95 +34,6 @@ app.use("/", require("./routes/visionMath"))
 app.use("/", require("./routes/smartGrade"))
 
 
-// app.post("/translate", async (req, res) => {
-//   const { message, language } = req.body;
-//   const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-//   const openai = new OpenAIApi(configuration);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: `Translate this into ${language}: ${message}`,
-//     max_tokens: 2048,
-//     temperature: 0.7,
-//   });
-//   res.send(response.data.choices[0].text);
-// });
-
-// app.post("/textValidation", async (req, res) => {
-//   const { content, prompt } = req.body;
-//   const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-//   const openai = new OpenAIApi(configuration);
-//   const correct_response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: `${prompt} ${content} `,
-//     max_tokens: 2048,
-//     temperature: 1,
-//   });
-//   res.send(correct_response.data.choices[0].text);
-// });
-
-// app.post("/assessmentCreator", async (req, res) => {
-//   const { text, formatType, questionFormat, temperature } = req.body;
-//   const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-
-//   const openai = new OpenAIApi(configuration);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     // prompt: `Step 1: Create mcq assessments from the article: ${text}. Step 2: Write generated mcq assessments in ${formatType}.`,
-//     prompt: `Create all posible ${questionFormat} and answers from ${text} in ${formatType}.`,
-//     max_tokens: 2048,
-//     temperature: temperature,
-//     top_p: 1.0,
-//     frequency_penalty: 0,
-//     presence_penalty: 0,
-//   });
-//   res.send(response.data.choices[0].text);
-// });
-
-// app.post("/visionMath", async (req, res) => {
-//   const { url } = req.body;
-//   const response = await axios.post(
-//     "https://api.mathpix.com/v3/text",
-//     {
-//       src: url,
-//       formats: ["text", "data", "html"],
-//       data_options: {
-//         include_asciimath: true,
-//         include_latex: true,
-//         include_mathml: true,
-//       },
-//     },
-//     {
-//       headers: {
-//         app_id: process.env.VISIONMATH_API_ID,
-//         app_key:
-//           process.env.VISIONMATH_API_KEY,
-//       },
-//     }
-//   );
-//   res.send(response.data);
-// });
-
-// app.post("/latexToText", async (req, res) => {
-//   const { latex } = req.body;
-//   const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-//   const openai = new OpenAIApi(configuration);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: `Convert Latex in words : ${latex}`,
-//     max_tokens: 2048,
-//     temperature: 1,
-//   });
-//   res.send(response.data.choices[0].text);
-// });
-
 app.post("/convertToQuestion", async (req, res) => {
   const { html } = req.body;
   const configuration = new Configuration({
@@ -139,27 +50,6 @@ app.post("/convertToQuestion", async (req, res) => {
   res.send(response.data.choices[0].text);
 });
 
-// app.post("/gradeAnswer", async (req, res) => {
-//   const { question, answer } = req.body;
-//   const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-//   const openai = new OpenAIApi(configuration);
-//   const response = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     // prompt: `Give correct grades in percentage on the basis of answer:${answer} for the question:${question}`,
-//     prompt: `Grade the answer from 0 to 5 based on its accuracy: \n\nQuestion:${question} \nAnswer:${answer} \n\nGrading:`,
-//     // prompt:`Please rate exact accuracy of the following answer on a scale of 0 to 5, where 0 represents the lowest score and 5 represents the highest score and give response in number only:
-//     // Question: ${question}
-//     // Answer: ${answer}`,
-//     max_tokens: 60,
-//     temperature: 0,
-//     top_p: 1.0,
-//     frequency_penalty: 0,
-//     presence_penalty: 0,
-//   });
-//   res.send(response.data.choices[0].text);
-// });
 
 app.post("/sendOtp", async (req, res) => {
   const { email, otp } = req.body;

4e5e21e9b20f210782b4f9eaa6034d23c4f7fb2d
gauravvermaqbs <gaurav.verma2@qbslearning.com>
2023-06-05 17:43:44 +0530

    google translate api added

--- server/routes/translate.js
+++ server/routes/translate.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const router = express.Router();
 const dotenv = require("dotenv");
+const translate = require("translate-google");
 dotenv.config();
-// console.log(process.env.OPENAI_API_KEY)
 
 router.post("/translate", async (req, res) => {
   const { message, language } = req.body;
@@ -13,19 +13,35 @@ router.post("/translate", async (req, res) => {
   });
   const openai = new OpenAIApi(configuration);
   // try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Translate this into ${language}: ${message}`,
-      max_tokens: 1000,
-      temperature: 0.3,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    res.send(response.data.choices[0].text);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Translate this into ${language}: ${message}`,
+    max_tokens: 1000,
+    temperature: 0.3,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+  res.send(response.data.choices[0].text);
   // } catch (err) {
-  //   console.error(err); 
+  //   console.error(err);
   // }
 });
 
-module.exports = router; 
+router.post("/translate-google", async (req, res) => {
+  const { message, language } = req.body;
+  try {
+    const translation = await translate(message, {
+      from: "auto",
+      to: language,
+    });
+    let regex = /&( nbsp|amp|quot|lt|gt);/g;
+    let result = translation.replace(regex, "");
+    res.json(result);
+  } catch (error) {
+    console.error("Translation error:", error);
+    res.status(500).json({ error: "An error occurred while translating." });
+  }
+});
+
+module.exports = router;
